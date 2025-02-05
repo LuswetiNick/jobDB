aed28669461e09e0873daef84421a03e58c5f2c1
LuswetiNick <nicklusweti@protonmail.com>
2025-02-05 15:44:22 +0300

    Add Google auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,16 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 });
